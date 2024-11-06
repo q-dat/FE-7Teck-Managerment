5eef48e4558128934252c4891e3cd60d6122420c
q-dat <quocdastore79@gmail.com>
2024-11-07 01:05:47 +0700

    feat: Implement dynamic visibility for scroll buttons for HomePage in UserPage

--- src/pages/user/HomePage.tsx
+++ src/pages/user/HomePage.tsx
@@ -1,12 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { BannerDesktop, BannerTablet, BannerMobile } from '../../assets/images';
 import HeaderResponsive from '../../components/UserPage/HeaderResponsive';
 import { IProduct } from '../../types/type/product/product';
 import { Button } from 'react-daisyui';
-import {
-  IoIosArrowDropleftCircle,
-  IoIosArrowDroprightCircle
-} from 'react-icons/io';
+import { MdArrowBackIosNew } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
 import { Link } from 'react-router-dom';
 
 const HomePage: React.FC = () => {
@@ -124,30 +122,138 @@ const HomePage: React.FC = () => {
   ];
 
   // const saleProducts = products.filter(product => product.status === 'sale');
+  const [isLeftButtonVisibleMobile, setIsLeftButtonVisibleMobile] =
+    useState(true);
+  const [isRightButtonVisibleMobile, setIsRightButtonVisibleMobile] =
+    useState(true);
+
+  const [isLeftButtonVisibleWindow, setIsLeftButtonVisibleWindow] =
+    useState(true);
+  const [isRightButtonVisibleWindow, setIsRightButtonVisibleWindow] =
+    useState(true);
+
+  const [isLeftButtonVisibleIpad, setIsLeftButtonVisibleIpad] = useState(true);
+  const [isRightButtonVisibleIpad, setIsRightButtonVisibleIpad] =
+    useState(true);
+
+  const [isLeftButtonVisibleMacbook, setIsLeftButtonVisibleMacbook] =
+    useState(true);
+  const [isRightButtonVisibleMacbook, setIsRightButtonVisibleMacbook] =
+    useState(true);
+
+  const scrollRefMobile = useRef<HTMLDivElement>(null);
+  const scrollRefWindow = useRef<HTMLDivElement>(null);
+  const scrollRefIpad = useRef<HTMLDivElement>(null);
+  const scrollRefMacbook = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScrollAndResize = () => {
+      checkScrollPosition(
+        scrollRefMobile.current,
+        setIsLeftButtonVisibleMobile,
+        setIsRightButtonVisibleMobile
+      );
+      checkScrollPosition(
+        scrollRefWindow.current,
+        setIsLeftButtonVisibleWindow,
+        setIsRightButtonVisibleWindow
+      );
+      checkScrollPosition(
+        scrollRefIpad.current,
+        setIsLeftButtonVisibleIpad,
+        setIsRightButtonVisibleIpad
+      );
+      checkScrollPosition(
+        scrollRefMacbook.current,
+        setIsLeftButtonVisibleMacbook,
+        setIsRightButtonVisibleMacbook
+      );
+    };
+
+    handleScrollAndResize();
+    window.addEventListener('resize', handleScrollAndResize);
+
+    if (scrollRefMobile.current) {
+      scrollRefMobile.current.addEventListener('scroll', handleScrollAndResize);
+    }
+    if (scrollRefWindow.current) {
+      scrollRefWindow.current.addEventListener('scroll', handleScrollAndResize);
+    }
+    if (scrollRefIpad.current) {
+      scrollRefIpad.current.addEventListener('scroll', handleScrollAndResize);
+    }
+    if (scrollRefMacbook.current) {
+      scrollRefMacbook.current.addEventListener(
+        'scroll',
+        handleScrollAndResize
+      );
+    }
+
+    return () => {
+      window.removeEventListener('resize', handleScrollAndResize);
+
+      if (scrollRefMobile.current) {
+        scrollRefMobile.current.removeEventListener(
+          'scroll',
+          handleScrollAndResize
+        );
+      }
+      if (scrollRefWindow.current) {
+        scrollRefWindow.current.removeEventListener(
+          'scroll',
+          handleScrollAndResize
+        );
+      }
+      if (scrollRefIpad.current) {
+        scrollRefIpad.current.removeEventListener(
+          'scroll',
+          handleScrollAndResize
+        );
+      }
+      if (scrollRefMacbook.current) {
+        scrollRefMacbook.current.removeEventListener(
+          'scroll',
+          handleScrollAndResize
+        );
+      }
+    };
+  }, []);
+
+  const checkScrollPosition = (
+    scrollContainer: HTMLElement | null,
+    setLeftButtonVisible: React.Dispatch<React.SetStateAction<boolean>>,
+    setRightButtonVisible: React.Dispatch<React.SetStateAction<boolean>>
+  ) => {
+    if (scrollContainer) {
+      const isAtStart = scrollContainer.scrollLeft === 0;
+      const isAtEnd =
+        scrollContainer.scrollLeft + scrollContainer.clientWidth >=
+        scrollContainer.scrollWidth;
+
+      setLeftButtonVisible(!isAtStart);
+      setRightButtonVisible(!isAtEnd);
+    }
+  };
 
   // scrollRefMobile
-  const scrollRefMobile = useRef<HTMLDivElement>(null);
   const scrollMobile = (scrollOffset: number) => {
     if (scrollRefMobile.current) {
       scrollRefMobile.current.scrollLeft += scrollOffset;
     }
   };
   // scrollRefIpad
-  const scrollRefIpad = useRef<HTMLDivElement>(null);
   const scrollIpad = (scrollOffset: number) => {
     if (scrollRefIpad.current) {
       scrollRefIpad.current.scrollLeft += scrollOffset;
     }
   };
   // scrollRefWindow
-  const scrollRefWindow = useRef<HTMLDivElement>(null);
   const scrollWindow = (scrollOffset: number) => {
     if (scrollRefWindow.current) {
       scrollRefWindow.current.scrollLeft += scrollOffset;
     }
   };
   // scrollRefMacbook
-  const scrollRefMacbook = useRef<HTMLDivElement>(null);
   const scrollMacbook = (scrollOffset: number) => {
     if (scrollRefMacbook.current) {
       scrollRefMacbook.current.scrollLeft += scrollOffset;
@@ -198,16 +304,16 @@ const HomePage: React.FC = () => {
           <Link to="product-detail">
             <div
               ref={scrollRefMobile}
-              className="grid grid-flow-col grid-rows-2 items-start justify-between gap-3 xl:gap-5 overflow-x-auto scroll-smooth py-1 scrollbar-hide"
+              className="grid grid-flow-col grid-rows-2 items-start justify-between gap-3 overflow-x-auto scroll-smooth py-1 scrollbar-hide xl:gap-5"
             >
               {products.map(product => (
                 <div
                   key={product._id}
                   className="dropdown dropdown-hover relative rounded-md bg-white shadow-headerMenu shadow-gray-50"
                 >
-                  <div className="flex w-[185px]  flex-col items-center justify-center xl:w-[220px]">
+                  <div className="flex w-[185px] flex-col items-center justify-center xl:w-[220px]">
                     <img
-                      className="h-[185px] w-[185px]  rounded-md object-cover xl:h-[220px] xl:w-[220px]"
+                      className="h-[185px] w-[185px] rounded-md object-cover xl:h-[220px] xl:w-[220px]"
                       src={product.img}
                     />
                     <p>{product.name}</p>
@@ -225,15 +331,15 @@ const HomePage: React.FC = () => {
           <div className="absolute top-1/2 flex w-full items-center justify-between">
             <Button
               onClick={() => scrollMobile(-200)}
-              className="rounded-full border-transparent bg-transparent p-0 text-primary shadow-none hover:border hover:border-primary dark:text-primary"
+              className={`rounded-full border-none bg-primary bg-opacity-80  py-0 px-[5px] text-white shadow-none dark:text-primary ${isLeftButtonVisibleMobile ? '' : 'bg-transparent text-transparent'}`}
             >
-              <IoIosArrowDropleftCircle className="text-4xl" />
+              <MdArrowBackIosNew className="text-4xl" />
             </Button>
             <Button
               onClick={() => scrollMobile(200)}
-              className="rounded-full border-transparent bg-transparent p-0 text-primary shadow-none hover:border hover:border-primary dark:text-primary"
+              className={`rounded-full border-none bg-primary bg-opacity-80  py-0 px-[5px] text-white shadow-none dark:text-primary ${isRightButtonVisibleMobile ? '' : 'bg-transparent text-transparent'}`}
             >
-              <IoIosArrowDroprightCircle className="text-4xl" />
+              <MdArrowForwardIos className="text-4xl" />
             </Button>
           </div>
         </div>
@@ -244,16 +350,16 @@ const HomePage: React.FC = () => {
           </p>
           <div
             ref={scrollRefIpad}
-            className="grid grid-flow-col grid-rows-2 items-start justify-between gap-3 xl:gap-5 overflow-x-auto scroll-smooth py-1 scrollbar-hide"
+            className="grid grid-flow-col grid-rows-2 items-start justify-between gap-3 overflow-x-auto scroll-smooth py-1 scrollbar-hide xl:gap-5"
           >
             {products.map(product => (
               <div
                 key={product._id}
                 className="dropdown dropdown-hover relative rounded-md bg-white shadow-headerMenu shadow-gray-50"
               >
-                <div className="flex w-[185px]  flex-col items-center justify-center xl:w-[220px]">
+                <div className="flex w-[185px] flex-col items-center justify-center xl:w-[220px]">
                   <img
-                    className="h-[185px] w-[185px]  rounded-md object-cover xl:h-[220px] xl:w-[220px]"
+                    className="h-[185px] w-[185px] rounded-md object-cover xl:h-[220px] xl:w-[220px]"
                     src={product.img}
                   />
                   <p>{product.name}</p>
@@ -270,15 +376,15 @@ const HomePage: React.FC = () => {
           <div className="absolute top-1/2 flex w-full items-center justify-between">
             <Button
               onClick={() => scrollIpad(-200)}
-              className="rounded-full border-transparent bg-transparent p-0 text-primary shadow-none hover:border hover:border-primary dark:text-primary"
+              className={`rounded-full border-none bg-primary bg-opacity-80  py-0 px-[5px] text-white shadow-none dark:text-primary ${isLeftButtonVisibleIpad ? '' : 'bg-transparent text-transparent'}`}
             >
-              <IoIosArrowDropleftCircle className="text-4xl" />
+              <MdArrowBackIosNew className="text-4xl" />
             </Button>
             <Button
               onClick={() => scrollIpad(200)}
-              className="rounded-full border-transparent bg-transparent p-0 text-primary shadow-none hover:border hover:border-primary dark:text-primary"
+              className={`rounded-full border-none bg-primary bg-opacity-80  py-0 px-[5px] text-white shadow-none dark:text-primary ${isRightButtonVisibleIpad ? '' : 'bg-transparent text-transparent'}`}
             >
-              <IoIosArrowDroprightCircle className="text-4xl" />
+              <MdArrowForwardIos className="text-4xl" />
             </Button>
           </div>
         </div>
@@ -289,16 +395,16 @@ const HomePage: React.FC = () => {
           </p>
           <div
             ref={scrollRefWindow}
-            className="flex flex-row items-start justify-between gap-3 xl:gap-5 overflow-x-auto scroll-smooth py-1 scrollbar-hide"
+            className="flex flex-row items-start justify-between gap-3 overflow-x-auto scroll-smooth py-1 scrollbar-hide xl:gap-5"
           >
             {products.map(product => (
               <div
                 key={product._id}
                 className="dropdown dropdown-hover relative rounded-md bg-white shadow-headerMenu shadow-gray-50"
               >
-                <div className="flex w-[185px]  flex-col items-center justify-center xl:w-[220px]">
+                <div className="flex w-[185px] flex-col items-center justify-center xl:w-[220px]">
                   <img
-                    className="h-[185px] w-[185px]  rounded-md object-cover xl:h-[220px] xl:w-[220px]"
+                    className="h-[185px] w-[185px] rounded-md object-cover xl:h-[220px] xl:w-[220px]"
                     src={product.img}
                   />
                   <p>{product.name}</p>
@@ -315,15 +421,15 @@ const HomePage: React.FC = () => {
           <div className="absolute top-1/2 flex w-full items-center justify-between">
             <Button
               onClick={() => scrollWindow(-200)}
-              className="rounded-full border-transparent bg-transparent p-0 text-primary shadow-none hover:border hover:border-primary dark:text-primary"
+              className={`rounded-full border-none bg-primary bg-opacity-80  py-0 px-[5px] text-white shadow-none dark:text-primary ${isLeftButtonVisibleWindow ? '' : 'bg-transparent text-transparent'}`}
             >
-              <IoIosArrowDropleftCircle className="text-4xl" />
+              <MdArrowBackIosNew className="text-4xl" />
             </Button>
             <Button
               onClick={() => scrollWindow(200)}
-              className="rounded-full border-transparent bg-transparent p-0 text-primary shadow-none hover:border hover:border-primary dark:text-primary"
+              className={`rounded-full border-none bg-primary bg-opacity-80  py-0 px-[5px] text-white shadow-none dark:text-primary ${isRightButtonVisibleWindow ? '' : 'bg-transparent text-transparent'}`}
             >
-              <IoIosArrowDroprightCircle className="text-4xl" />
+              <MdArrowForwardIos className="text-4xl" />
             </Button>
           </div>
         </div>
@@ -334,16 +440,16 @@ const HomePage: React.FC = () => {
           </p>
           <div
             ref={scrollRefMacbook}
-            className="flex flex-row items-start justify-between gap-3 xl:gap-5 overflow-x-auto scroll-smooth py-1 scrollbar-hide"
+            className="flex flex-row items-start justify-between gap-3 overflow-x-auto scroll-smooth py-1 scrollbar-hide xl:gap-5"
           >
             {products.map(product => (
               <div
                 key={product._id}
                 className="dropdown dropdown-hover relative rounded-md bg-white shadow-headerMenu shadow-gray-50"
               >
-                <div className="flex w-[185px]  flex-col items-center justify-center xl:w-[220px]">
+                <div className="flex w-[185px] flex-col items-center justify-center xl:w-[220px]">
                   <img
-                    className="h-[185px] w-[185px]  rounded-md object-cover xl:h-[220px] xl:w-[220px]"
+                    className="h-[185px] w-[185px] rounded-md object-cover xl:h-[220px] xl:w-[220px]"
                     src={product.img}
                   />
                   <p>{product.name}</p>
@@ -360,15 +466,15 @@ const HomePage: React.FC = () => {
           <div className="absolute top-1/2 flex w-full items-center justify-between">
             <Button
               onClick={() => scrollMacbook(-200)}
-              className="rounded-full border-transparent bg-transparent p-0 text-primary shadow-none hover:border hover:border-primary dark:text-primary"
+              className={`rounded-full border-none bg-primary bg-opacity-80  py-0 px-[5px] text-white shadow-none dark:text-primary ${isLeftButtonVisibleMacbook ? '' : 'bg-transparent text-transparent'}`}
             >
-              <IoIosArrowDropleftCircle className="text-4xl" />
+              <MdArrowBackIosNew className="text-4xl" />
             </Button>
             <Button
               onClick={() => scrollMacbook(200)}
-              className="rounded-full border-transparent bg-transparent p-0 text-primary shadow-none hover:border hover:border-primary dark:text-primary"
+              className={`rounded-full border-none bg-primary bg-opacity-80  py-0 px-[5px] text-white shadow-none dark:text-primary ${isRightButtonVisibleMacbook ? '' : 'bg-transparent text-transparent'}`}
             >
-              <IoIosArrowDroprightCircle className="text-4xl" />
+              <MdArrowForwardIos className="text-4xl" />
             </Button>
           </div>
         </div>
