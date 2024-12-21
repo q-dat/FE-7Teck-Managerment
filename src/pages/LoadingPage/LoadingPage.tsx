c8c142ca334353bb06bab3b1e9a476ab726dd54b
q-dat <quocdastore79@gmail.com>
2024-12-21 18:37:47 +0700

    chore: LoadingPage in UserPage

--- src/pages/LoadingPage/LoadingPage.tsx
+++ src/pages/LoadingPage/LoadingPage.tsx
@@ -1,9 +1,14 @@
 import BarLoader from 'react-spinners/BarLoader';
+import { Logo } from '../../assets/images';
 
 function LoadingPage({ loading }: { loading: boolean }) {
   return (
     <div className="relative flex h-screen flex-col items-center justify-center bg-white py-16">
       <div className="mt-8 flex flex-col items-center">
+        <img
+          src={Logo}
+          className="w-[200px] animate-bounce rounded-full"
+        />
         <BarLoader width={200} loading={loading} color="#e00303" />
       </div>
     </div>
