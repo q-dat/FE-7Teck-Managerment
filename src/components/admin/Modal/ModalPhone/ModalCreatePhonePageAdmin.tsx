9fbb41ef5d3e1e5269b1ee3d7421094c62801430
q-dat <quocdastore79@gmail.com>
2024-12-22 20:28:03 +0700

    chore: ModalCreatePhonePageAdmin in PhoneManager

--- src/components/admin/Modal/ModalPhone/ModalCreatePhonePageAdmin.tsx
+++ src/components/admin/Modal/ModalPhone/ModalCreatePhonePageAdmin.tsx
@@ -5,7 +5,7 @@ import InputModal from '../../InputModal';
 import { Toastify } from '../../../../helper/Toastify';
 import { PhoneContext } from '../../../../context/phone/PhoneContext';
 import { IPhone } from '../../../../types/type/phone/phone';
-import Select, { MultiValue } from 'react-select';
+import Select from 'react-select';
 
 interface ModalCreatePhoneProps {
   isOpen: boolean;
@@ -21,12 +21,53 @@ const ModalCreatePhonePageAdmin: React.FC<ModalCreatePhoneProps> = ({
   const isLoading = loading.create;
   const { register, handleSubmit, reset, setValue } = useForm<IPhone>();
 
-  const wifiOptions = [
-    { value: 'Wi-Fi 5', label: 'Wi-Fi 5' },
-    { value: 'Wi-Fi 6', label: 'Wi-Fi 6' }
-  ];
+  const optionsData = {
+    rear_camera_video: [
+      { value: '4K@30fps', label: '4K@30fps' },
+      { value: '1080p@60fps', label: '1080p@60fps' }
+    ],
+    rear_camera_features: [
+      { value: 'Auto Focus', label: 'Auto Focus' },
+      { value: 'HDR', label: 'HDR' }
+    ],
+    front_camera_features: [
+      { value: 'Portrait Mode', label: 'Portrait Mode' },
+      { value: 'Beauty Mode', label: 'Beauty Mode' }
+    ],
+    battery_technology: [
+      { value: 'Fast Charging', label: 'Fast Charging' },
+      { value: 'Wireless Charging', label: 'Wireless Charging' }
+    ],
+    advanced_security: [
+      { value: 'Face ID', label: 'Face ID' },
+      { value: 'Fingerprint Scanner', label: 'Fingerprint Scanner' }
+    ],
+    special_features: [
+      { value: '5G Support', label: '5G Support' },
+      { value: 'Dual SIM', label: 'Dual SIM' }
+    ],
+    wifiOptions: [
+      { value: 'Wi-Fi 5', label: 'Wi-Fi 5' },
+      { value: 'Wi-Fi 6', label: 'Wi-Fi 6' }
+    ],
+    gpsOptions: [
+      { value: 'GPS', label: 'GPS' },
+      { value: 'GLONASS', label: 'GLONASS' }
+    ],
+    voice_recording: [
+      { value: 'Stereo Recording', label: 'Stereo Recording' },
+      { value: 'Mono Recording', label: 'Mono Recording' }
+    ],
+    radio: [
+      { value: 'FM Radio', label: 'FM Radio' },
+      { value: 'AM Radio', label: 'AM Radio' }
+    ],
+    music_playback: [
+      { value: 'MP3', label: 'MP3' },
+      { value: 'AAC', label: 'AAC' }
+    ]
+  };
 
-  // Hàm xử lý submit
   const onSubmit: SubmitHandler<IPhone> = async formData => {
     const data = new FormData();
     data.append('name', formData.name);
@@ -35,7 +76,6 @@ const ModalCreatePhonePageAdmin: React.FC<ModalCreatePhoneProps> = ({
     data.append('price', formData.price.toString());
     data.append('des', formData.des || '');
 
-    // Thêm các file ảnh (nếu có)
     if (formData.img && formData.img[0]) {
       data.append('img', formData.img[0]);
     }
@@ -43,11 +83,9 @@ const ModalCreatePhonePageAdmin: React.FC<ModalCreatePhoneProps> = ({
       data.append('thumbnail', formData.thumbnail[0]);
     }
 
-
-    // Xử lý các mảng khác như wifi, gps...
     if (formData.connectivity?.wifi) {
       formData.connectivity.wifi.forEach(wifi => {
-        data.append('connectivity[wifi][]', wifi); // Chỉ cần lưu giá trị wifi (chuỗi)
+        data.append('connectivity[wifi][]', wifi);
       });
     }
 
@@ -63,87 +101,365 @@ const ModalCreatePhonePageAdmin: React.FC<ModalCreatePhoneProps> = ({
     }
   };
 
-  const handleOverlayClick = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    if ((e.target as HTMLElement).classList.contains('modal-overlay')) {
-      onClose();
-    }
-  };
-
   if (!isOpen) return null;
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div
-        onClick={handleOverlayClick}
-        className="modal-overlay fixed inset-0 z-50 flex w-full items-center justify-center bg-black bg-opacity-40"
-      >
-        <div
-          onClick={e => e.stopPropagation()}
-          className="mx-2 flex w-[400px] flex-col rounded-lg bg-white p-5 text-start shadow dark:bg-gray-800"
-        >
-          <div>
-            <p className="font-bold text-black dark:text-white">
-              Tạo sản phẩm mới
-            </p>
-            {/* Các trường đầu vào */}
-            <InputModal
-              type="text"
-              {...register('name', { required: true })}
-              placeholder="Tên sản phẩm"
-            />
-            <InputModal
-              type="text"
-              {...register('phone_catalog_id', { required: true })}
-              placeholder="Danh mục"
-            />
-            <InputModal
-              type="text"
-              {...register('status')}
-              placeholder="Trạng thái"
-            />
-            <InputModal
-              type="number"
-              {...register('price', { required: true })}
-              placeholder="Giá"
-            />
-            <InputModal
-              type="text"
-              {...register('des')}
-              placeholder="Mô tả sản phẩm"
-            />
-            <InputModal
-              type="file"
-              {...register('img', { required: true })}
-              placeholder="Ảnh sản phẩm"
-            />
-            <InputModal
-              type="file"
-              {...register('thumbnail')}
-              placeholder="Ảnh thumbnail"
-            />
-
-
-            {/* React Select cho Wi-Fi */}
-            <div className="my-2">
-              <label className="block text-sm">Wi-Fi</label>
-              <Select
-                isMulti
-                name="wifi"
-                options={wifiOptions}
-                onChange={(
-                  selectedOptions: MultiValue<{ value: string; label: string }>
-                ) => {
-                  const selectedValues = selectedOptions.map(
-                    wifi => wifi.value
-                  ); // Lấy giá trị 'value'
-                  setValue('connectivity.wifi', selectedValues); // Cập nhật form với mảng chuỗi
-                }}
+      <div className="modal-overlay fixed inset-0 z-50 flex w-full items-center justify-center bg-black bg-opacity-40">
+        <div className="mx-2 flex w-full flex-col rounded-lg bg-white p-5 text-start shadow dark:bg-gray-800">
+          <p className="font-bold text-black dark:text-white">
+            Tạo sản phẩm mới
+          </p>
+          <div className="h-[500px] w-full overflow-y-auto">
+            {/* Các trường cơ bản */}
+            <div className="">
+              <InputModal
+                type="text"
+                {...register('name', { required: true })}
+                placeholder="Tên sản phẩm"
+              />
+              <InputModal
+                type="text"
+                {...register('phone_catalog_id', { required: true })}
+                placeholder="Danh mục"
+              />
+              <InputModal
+                type="text"
+                {...register('status')}
+                placeholder="Trạng thái"
+              />
+              <InputModal
+                type="number"
+                {...register('price', { required: true })}
+                placeholder="Giá"
+              />
+              <InputModal
+                type="text"
+                {...register('des')}
+                placeholder="Mô tả sản phẩm"
+              />
+              <InputModal
+                type="file"
+                {...register('img', { required: true })}
+                placeholder="Ảnh sản phẩm"
+              />
+              <InputModal
+                type="file"
+                {...register('thumbnail')}
+                placeholder="Ảnh thumbnail"
+              />
+            </div>
+            {/* Trường cấu hình và bộ nhớ */}
+            <div className="">
+              <InputModal
+                type="text"
+                {...register('configuration_and_memory.operating_system')}
+                placeholder="Hệ điều hành"
+              />
+              <InputModal
+                type="text"
+                {...register('configuration_and_memory.cpu_chip')}
+                placeholder="Chip xử lý CPU"
+              />
+              <InputModal
+                type="text"
+                {...register('configuration_and_memory.ram')}
+                placeholder="RAM"
+              />
+              {/* Mảng cho Rear Camera Features */}
+              <div className="my-2">
+                <label className="block text-sm">Rear Camera Features</label>
+                <Select
+                  isMulti
+                  options={optionsData.rear_camera_features}
+                  onChange={selected =>
+                    setValue(
+                      'camera_and_screen.rear_camera_features',
+                      selected.map(option => option.value)
+                    )
+                  }
+                />
+              </div>
+            </div>
+            {/* Trường camera và màn hình */}
+            <div className="">
+              <InputModal
+                type="text"
+                {...register('camera_and_screen.rear_camera_resolution')}
+                placeholder="Độ phân giải camera sau"
+              />
+              <div className="my-2">
+                <label className="block text-sm">Quay phim camera sau</label>
+                <Select
+                  isMulti
+                  options={optionsData.rear_camera_video}
+                  onChange={selected =>
+                    setValue(
+                      'camera_and_screen.rear_camera_video',
+                      selected.map(option => option.value)
+                    )
+                  }
+                />
+              </div>
+              <InputModal
+                type="text"
+                {...register('camera_and_screen.rear_camera_flash')}
+                placeholder="Đèn Flash camera sau"
+              />
+              <div className="my-2">
+                <label className="block text-sm">Tính năng camera sau</label>
+                <Select
+                  isMulti
+                  options={optionsData.rear_camera_features}
+                  onChange={selected =>
+                    setValue(
+                      'camera_and_screen.rear_camera_features',
+                      selected.map(option => option.value)
+                    )
+                  }
+                />
+              </div>
+              <InputModal
+                type="text"
+                {...register('camera_and_screen.front_camera_resolution')}
+                placeholder="Độ phân giải camera trước"
+              />
+              <div className="my-2">
+                <label className="block text-sm">Tính năng camera trước</label>
+                <Select
+                  isMulti
+                  options={optionsData.front_camera_features}
+                  onChange={selected =>
+                    setValue(
+                      'camera_and_screen.front_camera_features',
+                      selected.map(option => option.value)
+                    )
+                  }
+                />
+              </div>
+              <InputModal
+                type="text"
+                {...register('camera_and_screen.screen_technology')}
+                placeholder="Công nghệ màn hình"
+              />
+              <InputModal
+                type="text"
+                {...register('camera_and_screen.screen_resolution')}
+                placeholder="Độ phân giải màn hình"
+              />
+              <InputModal
+                type="text"
+                {...register('camera_and_screen.screen_size')}
+                placeholder="Màn hình rộng"
+              />
+              <InputModal
+                type="text"
+                {...register('camera_and_screen.max_brightness')}
+                placeholder="Độ sáng tối đa"
+              />
+              <InputModal
+                type="text"
+                {...register('camera_and_screen.touchscreen_glass')}
+                placeholder="Mặt kính cảm ứng"
+              />
+            </div>
+            {/* Trường pin và sạc */}
+            <div className="">
+              <InputModal
+                type="text"
+                {...register('battery_and_charging.battery_capacity')}
+                placeholder="Dung lượng pin"
+              />
+              <InputModal
+                type="text"
+                {...register('battery_and_charging.battery_type')}
+                placeholder="Loại pin"
+              />
+              <InputModal
+                type="text"
+                {...register('battery_and_charging.max_charging_support')}
+                placeholder="Hỗ trợ sạc tối đa"
+              />
+              <div className="my-2">
+                <label className="block text-sm">Công nghệ pin</label>
+                <Select
+                  isMulti
+                  options={optionsData.battery_technology}
+                  onChange={selected =>
+                    setValue(
+                      'battery_and_charging.battery_technology',
+                      selected.map(option => option.value)
+                    )
+                  }
+                />
+              </div>
+            </div>
+            {/* Trường tiện ích */}
+            <div className="">
+              <div className="my-2">
+                <label className="block text-sm">Bảo mật nâng cao</label>
+                <Select
+                  isMulti
+                  options={optionsData.advanced_security}
+                  onChange={selected =>
+                    setValue(
+                      'features.advanced_security',
+                      selected.map(option => option.value)
+                    )
+                  }
+                />
+              </div>
+              <div className="my-2">
+                <label className="block text-sm">Tính năng đặc biệt</label>
+                <Select
+                  isMulti
+                  options={optionsData.special_features}
+                  onChange={selected =>
+                    setValue(
+                      'features.special_features',
+                      selected.map(option => option.value)
+                    )
+                  }
+                />
+              </div>
+              <InputModal
+                type="text"
+                {...register('features.water_dust_resistant')}
+                placeholder="Kháng nước/bụi"
+              />
+              <div className="my-2">
+                <label className="block text-sm">Ghi âm</label>
+                <Select
+                  isMulti
+                  options={optionsData.voice_recording}
+                  onChange={selected =>
+                    setValue(
+                      'features.voice_recording',
+                      selected.map(option => option.value)
+                    )
+                  }
+                />
+              </div>
+              <div className="my-2">
+                <label className="block text-sm">Radio</label>
+                <Select
+                  isMulti
+                  options={optionsData.radio}
+                  onChange={selected =>
+                    setValue(
+                      'features.radio',
+                      selected.map(option => option.value)
+                    )
+                  }
+                />
+              </div>
+              <InputModal
+                type="text"
+                {...register('features.video_playback')}
+                placeholder="Xem phim"
+              />
+              <div className="my-2">
+                <label className="block text-sm">Nghe nhạc</label>
+                <Select
+                  isMulti
+                  options={optionsData.music_playback}
+                  onChange={selected =>
+                    setValue(
+                      'features.music_playback',
+                      selected.map(option => option.value)
+                    )
+                  }
+                />
+              </div>
+            </div>
+            {/* Trường kết nối */}
+            <div className="">
+              <InputModal
+                type="text"
+                {...register('connectivity.mobile_network')}
+                placeholder="Mạng di động"
+              />
+              <InputModal
+                type="text"
+                {...register('connectivity.sim')}
+                placeholder="Sim"
+              />
+              <div className="my-2">
+                <label className="block text-sm">Wi-Fi</label>
+                <Select
+                  isMulti
+                  options={optionsData.wifiOptions}
+                  onChange={selected =>
+                    setValue(
+                      'connectivity.wifi',
+                      selected.map(option => option.value)
+                    )
+                  }
+                />
+              </div>
+              <div className="my-2">
+                <label className="block text-sm">GPS</label>
+                <Select
+                  isMulti
+                  options={optionsData.gpsOptions}
+                  onChange={selected =>
+                    setValue(
+                      'connectivity.gps',
+                      selected.map(option => option.value)
+                    )
+                  }
+                />
+              </div>
+              <InputModal
+                type="text"
+                {...register('connectivity.bluetooth')}
+                placeholder="Bluetooth"
+              />
+              <InputModal
+                type="text"
+                {...register('connectivity.charging_connection_port')}
+                placeholder="Cổng kết nối/sạc"
+              />
+              <InputModal
+                type="text"
+                {...register('connectivity.headphone_jack')}
+                placeholder="Jack tai nghe"
+              />
+              <InputModal
+                type="text"
+                {...register('connectivity.other_connectivity')}
+                placeholder="Kết nối khác"
+              />
+            </div>
+            {/* Trường thiết kế và chất liệu */}
+            <div className="">
+              <InputModal
+                type="text"
+                {...register('design_and_material.design')}
+                placeholder="Thiết kế"
+              />
+              <InputModal
+                type="text"
+                {...register('design_and_material.material')}
+                placeholder="Chất liệu"
+              />
+              <InputModal
+                type="text"
+                {...register('design_and_material.dimensions_and_weight')}
+                placeholder="Kích thước và khối lượng"
+              />
+              <InputModal
+                type="text"
+                {...register('design_and_material.release_date')}
+                placeholder="Thời điểm ra mắt"
+              />
+              <InputModal
+                type="text"
+                {...register('design_and_material.brand')}
+                placeholder="Hãng"
               />
             </div>
           </div>
-
           <div className="flex flex-row items-center justify-center space-x-5 text-center">
             <Button onClick={onClose} className="border-gray-50 text-black">
               Hủy
@@ -154,14 +470,7 @@ const ModalCreatePhonePageAdmin: React.FC<ModalCreatePhoneProps> = ({
               type="submit"
               className="group text-white"
             >
-              {isLoading ? (
-                <div className="flex cursor-progress flex-row items-center justify-center bg-primary text-white group-hover:bg-opacity-10">
-                  <span>Đang tạo...</span>
-                  <span className="loading loading-spinner"></span>
-                </div>
-              ) : (
-                'Xác nhận'
-              )}
+              {isLoading ? 'Đang tạo...' : 'Xác nhận'}
             </Button>
           </div>
         </div>
