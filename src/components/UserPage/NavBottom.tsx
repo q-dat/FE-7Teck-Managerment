f559c9bf77680d93b1329d6c90639ed3c28cc524
q-dat <quocdastore79@gmail.com>
2024-12-24 17:01:09 +0700

    fix: NavBottom UI for UserPage

--- src/components/UserPage/NavBottom.tsx
+++ src/components/UserPage/NavBottom.tsx
@@ -1,35 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+const navLink = [
+  {
+    name: 'Giá thu cũ',
+    link: '/price-list'
+  },
+  {
+    name: 'IPhone',
+    link: '/phone-list'
+  },
+  {
+    name: 'Ipad',
+    link: '/ipad-list'
+  },
+  {
+    name: 'Window',
+    link: '/window-list'
+  },
+  {
+    name: 'Macbook',
+    link: '/macbook-list'
+  }
+];
 const NavBottom: React.FC = () => {
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-white bg-gradient-to-r from-primary to-black box-decoration-clone xl:hidden">
-      <div className="flex flex-row items-end justify-start text-xs text-white">
-        <Link to="/price-list">
-          <button className="bg-gradient-to-tr from-primary to-black p-2 uppercase">
-            Giá thu cũ
-          </button>
-        </Link>
-        <Link to="/phone-list">
-          <button className=" uppercase bg-gradient-to-tr from-primary to-black p-2">
-            IPhone
-          </button>
-        </Link>
-        <Link to="/ipad-list">
-          <button className=" uppercase bg-gradient-to-tr from-primary to-black p-2">
-            IPad
-          </button>
-        </Link>
-        <Link to="/window-list">
-          <button className=" uppercase bg-gradient-to-tr from-primary to-black p-2">
-            Window
-          </button>
-        </Link>
-        <Link to="/macbook-list">
-          <button className=" uppercase bg-gradient-to-tr from-primary to-black p-2">
-            Macbook
-          </button>
-        </Link>
+    <div className="fixed bottom-0 left-0 w-full xl:hidden">
+      <div className="flex w-full text-xs text-white ">
+        {navLink.map(item => (
+          <Link to={item.link} className="flex-grow">
+            <button className="h-full w-full bg-gradient-to-tr from-primary to-black p-2 uppercase">
+              {item.name}
+            </button>
+          </Link>
+        ))}
       </div>
     </div>
   );
