cf4157df188dec258b353e44f85b1c66b20289f5
q-dat <quocdastore79@gmail.com>
2024-12-27 16:11:38 +0700

    chore: style InputModal in AdminPage

--- src/components/admin/InputModal.tsx
+++ src/components/admin/InputModal.tsx
@@ -18,9 +18,13 @@ const InputModal = forwardRef<HTMLInputElement, InputModalProps>(
           value={value}
           type={type}
           name={name}
-          className="w-full rounded-md text-black dark:text-white border-none bg-white p-0 text-sm font-light placeholder-black focus:outline-none dark:bg-transparent dark:placeholder-white"
+          className="w-full rounded-md border-none bg-white p-0 text-sm font-light text-black placeholder-black focus:outline-none dark:bg-transparent dark:text-white dark:placeholder-white"
           placeholder={placeholder}
           onChange={onChange}
+          style={{
+            outline: 'none',
+            boxShadow: 'none'
+          }}
         />
       </div>
     );
