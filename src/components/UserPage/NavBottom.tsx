5a9848379474487c3588c6f5cc12c49623c797c9
q-dat <quocdastore79@gmail.com>
2024-12-28 15:05:48 +0700

    chore: NavBottom UI in UserPage

--- src/components/UserPage/NavBottom.tsx
+++ src/components/UserPage/NavBottom.tsx
@@ -1,38 +1,50 @@
 import React from 'react';
+import { FaMobileAlt, FaTabletAlt, FaWindows } from 'react-icons/fa';
+import { MdListAlt } from 'react-icons/md';
+import { RiMacbookFill } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 const navLink = [
   {
+    icon: MdListAlt,
     name: 'Giá thu cũ',
     link: '/price-list'
   },
   {
-    name: 'IPhone',
+    icon: FaMobileAlt,
+    name: 'iPhone',
     link: '/phone-list'
   },
   {
-    name: 'Ipad',
+    icon: FaTabletAlt,
+    name: 'ipad',
     link: '/ipad-list'
   },
   {
-    name: 'Window',
-    link: '/window-list'
-  },
-  {
+    icon: RiMacbookFill,
     name: 'Macbook',
     link: '/macbook-list'
+  },
+  {
+    icon: FaWindows,
+    name: 'Window',
+    link: '/window-list'
   }
 ];
 const NavBottom: React.FC = () => {
   return (
     <div className="fixed bottom-0 left-0 z-[99999] w-full xl:hidden">
       <div className="flex w-full justify-between gap-[1px] bg-white text-xs">
-        {navLink.map((item, index) => (
-          <Link key={index} to={item.link} className="flex-grow">
-            <button className="h-full w-full bg-black px-1 py-2 uppercase text-white">
-              {item.name}
-            </button>
-          </Link>
-        ))}
+        {navLink.map((item, index) => {
+          const Icon = item.icon;
+          return (
+            <Link key={index} to={item.link} className="flex-grow">
+              <button className="flex h-full w-full flex-col items-center justify-center bg-black p-1 text-white">
+                {Icon && <Icon className="text-lg" />}
+                {item.name}
+              </button>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
