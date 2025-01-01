9deba06bb37828ba36cc9f83f67d53ed4a46dfbb
q-dat <quocdastore79@gmail.com>
2025-01-01 17:40:57 +0700

    feat: add navigation from PhonePage to PhoneDetailPage

--- src/pages/user/PhonePage.tsx
+++ src/pages/user/PhonePage.tsx
@@ -1,13 +1,24 @@
 import React, { useContext, useState, useEffect } from 'react';
 import Pagination from '../../components/UserPage/Pagination';
 import HeaderResponsive from '../../components/UserPage/HeaderResponsive';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { PhoneCatalogContext } from '../../context/phone-catalog/PhoneCatalogContext';
 import { Button } from 'react-daisyui';
 
 const PhonePage: React.FC = () => {
   const { phoneCatalogs } = useContext(PhoneCatalogContext);
   const [currentPage, setCurrentPage] = useState(1);
+  const navigate = useNavigate();
+  const slugify = (text: string) => {
+    return text
+      .toString()
+      .normalize('NFD') // Chuyển sang Unicode
+      .replace(/\p{Diacritic}/gu, '') // Loại bỏ dấu
+      .toLowerCase() // Chuyển tất cả thành chữ thường
+      .replace(/[^a-z0-9]+/g, '-') // Thay thế khoảng trắng và ký tự không phải chữ cái bằng dấu gạch ngang
+      .replace(/^-+|-+$/g, ''); // Loại bỏ dấu gạch ngang ở đầu và cuối chuỗi
+  };
+  // Panigation
   const itemsPerPage = 12;
 
   const totalPages = Math.ceil(phoneCatalogs.length / itemsPerPage);
@@ -56,13 +67,17 @@ const PhonePage: React.FC = () => {
               {/* Danh Sách Điện Thoại */}
             </p>
             <div className="grid grid-flow-row grid-cols-2 items-start gap-[10px] md:grid-cols-4 xl:grid-cols-6">
-              {currentPhones.map(phone => (
-                <div
-                  key={phone?._id}
-                  className="flex h-full w-full flex-col justify-between rounded-md border border-[#f2f4f7] bg-white text-black"
-                >
-                  <Link to="phone-detail">
-                    <div className="flex flex-col items-start">
+              {currentPhones.map(phone => {
+                const phoneUrl = slugify(phone.name);
+                return (
+                  <div
+                    key={phone?._id}
+                    className="flex h-full w-full flex-col justify-between rounded-md border border-[#f2f4f7] bg-white text-black"
+                  >
+                    <div
+                      onClick={() => navigate(`/${phoneUrl}`)}
+                      className="flex flex-col items-start"
+                    >
                       <img
                         className="h-[200px] w-full rounded-[5px] rounded-b-none object-cover xl:h-[250px]"
                         src={phone?.img}
@@ -72,27 +87,27 @@ const PhonePage: React.FC = () => {
                         <p>Điện thoại {phone?.name}</p>
                       </div>
                     </div>
-                  </Link>
-                  {/*  */}
-                  <div className="flex flex-col items-start justify-center gap-1 p-1">
-                    <p className="text-gray-500">
-                      Từ:&nbsp;
-                      <span className="text-red-500">
-                        {(phone?.price * 1000).toLocaleString('vi-VN')}{' '}
-                        <sup>đ</sup>
-                      </span>
-                    </p>
-                    <Link to="checkout" className="z-50 w-full">
-                      <Button
-                        size="xs"
-                        className="w-full rounded-md border-none bg-primary bg-opacity-10 text-primary"
-                      >
-                        Mua Ngay
-                      </Button>
-                    </Link>
+                    {/*  */}
+                    <div className="flex flex-col items-start justify-center gap-1 p-1">
+                      <p className="text-gray-500">
+                        Từ:&nbsp;
+                        <span className="text-red-500">
+                          {(phone?.price * 1000).toLocaleString('vi-VN')}{' '}
+                          <sup>đ</sup>
+                        </span>
+                      </p>
+                      <Link to="checkout" className="z-50 w-full">
+                        <Button
+                          size="xs"
+                          className="w-full rounded-md border-none bg-primary bg-opacity-10 text-primary"
+                        >
+                          Mua Ngay
+                        </Button>
+                      </Link>
+                    </div>
                   </div>
-                </div>
-              ))}
+                );
+              })}
             </div>
           </div>
           {/* Pagination Controls */}
