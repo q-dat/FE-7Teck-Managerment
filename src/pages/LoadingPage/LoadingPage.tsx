c6e45282bffd9f29b938bdeb51432895b6495aaf
q-dat <quocdastore79@gmail.com>
2025-01-29 16:17:18 +0700

    chore: LoadingPage in UserPage

--- src/pages/LoadingPage/LoadingPage.tsx
+++ src/pages/LoadingPage/LoadingPage.tsx
@@ -4,6 +4,7 @@ function LoadingPage({ loading }: { loading: boolean }) {
   return (
     <div className="relative flex h-screen w-screen flex-col items-center justify-center bg-white py-16">
       <div className="mt-8 flex flex-col items-center">
+        <p className="text-sm text-gray-600">Vui lòng chờ...</p>
         <BarLoader width={200} loading={loading} color="#e00303" />
       </div>
     </div>
