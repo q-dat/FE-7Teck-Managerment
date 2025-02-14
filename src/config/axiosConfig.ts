197f148c2ef832343c86eb3ee2aa5aec2bcbad9a
q-dat <quocdatstore79@gmail.com>
2025-02-14 21:39:28 +0700

    chore: axiosConfig

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_PORT2 ||  import.meta.env.VITE_API_PORT, // URL backend từ .env
+  baseURL: import.meta.env.VITE_API_PORT, // URL backend từ .env
   withCredentials: true, // Bật gửi thông tin xác thực
   timeout: 10000
 });
