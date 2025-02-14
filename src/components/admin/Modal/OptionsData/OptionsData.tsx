a821c1d15f2f4a9b84a156f32f497c65bc131f6e
q-dat <quocdatstore79@gmail.com>
2025-02-14 17:27:38 +0700

    feat: add Modal OptionsDataUI

--- src/components/admin/Modal/OptionsData/OptionsData.tsx
+++ src/components/admin/Modal/OptionsData/OptionsData.tsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { IOptionPhoneData } from '../../../../types/type/optionsData/optionsPhoneData';
+import { OptionPhoneContext } from '../../../../context/optionsData/OptionsPhoneContext';
+import { Toastify } from '../../../../helper/Toastify';
+import { Button } from 'react-daisyui';
 interface ModalCreateAdminProps {
   isOpen: boolean;
   onClose: () => void;
 }
-
+const fields: (keyof IOptionPhoneData)[] = [
+  'rear_camera_video',
+  'rear_camera_features',
+  'front_camera_features',
+  'battery_technology',
+  'advanced_security',
+  'special_features',
+  'wifiOptions',
+  'gpsOptions',
+  'voice_recording',
+  'radio',
+  'music_playback'
+];
 const OptionsData: React.FC<ModalCreateAdminProps> = ({ isOpen, onClose }) => {
+  const {  handleSubmit, reset } = useForm<IOptionPhoneData>();
+  const { loading, getAllOptionPhones, createOptionPhone } =
+    useContext(OptionPhoneContext);
+  const isLoading = loading.create;
+
+  const onSubmit: SubmitHandler<IOptionPhoneData> = async formData => {
+    const data = new FormData();
+
+    fields.forEach(field => {
+      if (formData[field]) {
+        Object.entries(formData[field]).forEach(([key, value]) => {
+          if (Array.isArray(value)) {
+            value.forEach(item =>
+              data.append(`${field}[${key}][]`, item.value)
+            );
+          } else {
+            data.append(`${field}[${key}]`, value.value);
+          }
+        });
+      }
+    });
+    try {
+      await createOptionPhone(data);
+      reset();
+      getAllOptionPhones();
+      Toastify('Tạo danh mục thành công!', 201);
+      onClose();
+    } catch (err) {
+      Toastify(`Lỗi: ${err}`, 500);
+      console.error(err);
+    }
+  };
+
   const handleOverlayClick = (
     e: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
@@ -24,6 +74,20 @@ const OptionsData: React.FC<ModalCreateAdminProps> = ({ isOpen, onClose }) => {
         className="mx-2 flex w-full flex-col rounded-lg bg-white p-5 text-start shadow dark:bg-gray-800 xl:w-5/6"
       >
         <h1>Danh mục options thông tin sản phẩm!</h1>
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h1>OptionsPhoneDate</h1>
+
+            <Button
+              disabled={isLoading}
+              color="primary"
+              type="submit"
+              className="group text-white"
+            >
+              {isLoading ? 'Đang tạo...' : 'Xác nhận'}
+            </Button>
+          </form>
+        </div>
       </div>
     </div>
   );
