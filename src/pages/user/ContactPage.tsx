213046efb5020c3eb03f0427ee7dd0e8e9151ae7
q-dat <quocdatstore79@gmail.com>
2025-02-25 14:59:22 +0700

    chore: PurchasePage

--- src/pages/user/ContactPage.tsx
+++ src/pages/user/ContactPage.tsx
@@ -1,46 +1,9 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import HeaderResponsive from '../../components/UserPage/HeaderResponsive';
 import { Link } from 'react-router-dom';
-import { Textarea, Button } from 'react-daisyui';
-import InputForm from '../../components/UserPage/InputForm';
-import { Logo } from '../../assets/images';
 
 const ContactPage: React.FC = () => {
-  const [result, setResult] = React.useState<string>('');
-  const formRef = useRef<HTMLFormElement>(null);
-
-  const onSubmit = async (
-    event: React.FormEvent<HTMLFormElement>
-  ): Promise<void> => {
-    event.preventDefault();
-    setResult('Đang gửi...');
-
-    const formData = new FormData(event.currentTarget);
-
-    formData.append('access_key', ' ');
-
-    try {
-      const response = await fetch('https://api.web3forms.com/submit', {
-        method: 'POST',
-        body: formData
-      });
-
-      const data: { success: boolean; message: string } = await response.json();
-
-      if (data.success) {
-        setResult('Đã gửi thành công!');
-
-        // Reset form using formRef
-        formRef.current?.reset();
-      } else {
-        console.error('Error', data);
-        setResult(data.message);
-      }
-    } catch (error) {
-      console.error('Yêu cầu thất bại', error);
-      setResult('Đã xảy ra lỗi khi gửi biểu mẫu!');
-    }
-  };
+ 
   return (
     <div>
       <HeaderResponsive Title_NavbarMobile="Chính Sách Bảo Hành" />
@@ -59,65 +22,7 @@ const ContactPage: React.FC = () => {
             </li>
           </ul>
         </div>
-        {/* Form */}
-        <div className="px-2 xl:px-[100px]">
-          <form
-            ref={formRef}
-            onSubmit={onSubmit}
-            className="flex flex-col items-center justify-center gap-y-10 rounded-xl px-0 py-5 shadow-none dark:bg-white xl:flex-row xl:gap-x-10 xl:gap-y-0 xl:px-10 xl:py-10 xl:shadow-mainMenu"
-          >
-            <div className="">
-              <img
-                loading="lazy"
-                src={Logo}
-                alt="Logo"
-                className="h-full w-[150px] rounded-full border border-primary xl:w-[300px]"
-              />
-            </div>
-            <div
-              className="flex w-full flex-col gap-5"
-              role="region"
-              aria-label="Thông tin liên hệ"
-            >
-              <h1 className="text-2xl font-bold">Thông tin liên hệ:</h1>
-              <div className="flex w-full flex-col gap-5 xl:flex-row">
-                <div className="w-full">
-                  <InputForm
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    className="border border-gray-300 bg-white text-black focus:border-primary"
-                    classNameLabel="bg-white dark:peer-placeholder-shown:text-black dark:peer-focus:text-black"
-                  />
-                </div>
-                <div className="w-full">
-                  <InputForm
-                    name="name"
-                    type="text"
-                    className="border border-gray-300 bg-white text-black focus:border-primary"
-                    placeholder="Tên của bạn"
-                    classNameLabel="bg-white dark:peer-placeholder-shown:text-black dark:peer-focus:text-black"
-                  />
-                </div>
-              </div>
-              <Textarea
-                name="feedback"
-                className="border border-gray-300 bg-white pb-20 text-black focus:border-primary focus:outline-none"
-                placeholder="Bạn muốn đặt câu hỏi hay góp ý gì?"
-              />
-              <div className="w-full">
-                <Button
-                  aria-label="Nút: Gửi"
-                  className="w-full bg-primary text-sm text-white hover:border-primary hover:bg-secondary hover:text-white dark:hover:bg-opacity-50"
-                  type="submit"
-                >
-                  Gửi
-                </Button>
-                <span>{result}</span>
-              </div>
-            </div>
-          </form>
-        </div>
+    
       </div>
     </div>
   );
--- src/pages/user/PurchasePage.tsx
+++ src/pages/user/PurchasePage.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import HeaderResponsive from '../../components/UserPage/HeaderResponsive';
 import { Link } from 'react-router-dom';
+import { Textarea, Button } from 'react-daisyui';
+import InputForm from '../../components/UserPage/InputForm';
 
 const PurchasePage: React.FC = () => {
+  const [result, setResult] = React.useState<string>('');
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const onSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event.preventDefault();
+    setResult('Đang gửi...');
+
+    const formData = new FormData(event.currentTarget);
+
+    formData.append('access_key', import.meta.env.VITE_WEB3FORMS_KEY);
+
+    try {
+      const response = await fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        body: formData
+      });
+
+      const data: { success: boolean; message: string } = await response.json();
+
+      if (data.success) {
+        setResult('Đã gửi biểu mẫu thành công! Vui lòng đợi để được hỗ trợ!');
+
+        // Reset form using formRef
+        formRef.current?.reset();
+      } else {
+        console.error('Error', data);
+        setResult(data.message);
+      }
+    } catch (error) {
+      console.error('Yêu cầu thất bại', error);
+      setResult('Đã xảy ra lỗi khi gửi biểu mẫu!');
+    }
+  };
   return (
     <div>
       <HeaderResponsive Title_NavbarMobile="Mua Hàng" />
@@ -21,8 +58,59 @@ const PurchasePage: React.FC = () => {
             </li>
           </ul>
         </div>
-        {/*  */}
-        <div className="px-2 xl:px-20"></div>
+        {/* Form */}
+        <div className="mt-5 px-2 xl:px-20">
+          <form
+            ref={formRef}
+            onSubmit={onSubmit}
+            className="flex flex-col items-center justify-center gap-y-10 rounded-xl px-0 py-5 shadow-none dark:bg-white xl:flex-row xl:gap-x-10 xl:gap-y-0 xl:px-10 xl:py-10 xl:shadow-mainMenu"
+          >
+            <div
+              className="flex w-full flex-col gap-5"
+              role="region"
+              aria-label="Thông tin liên hệ"
+            >
+              <h1 className="text-2xl font-bold">
+                Liên Hệ Thanh Toán Trực Tiếp – Đơn Giản & Nhanh Chóng:
+              </h1>
+              <div className="flex w-full flex-col gap-5 xl:flex-row">
+                <div className="w-full">
+                  <InputForm
+                    name="Số điện thoại:"
+                    type="number"
+                    placeholder="Nhập số điện thoại"
+                    className="border border-gray-300 bg-white text-black focus:border-primary"
+                    classNameLabel="bg-white dark:peer-placeholder-shown:text-black dark:peer-focus:text-black"
+                  />
+                </div>
+                <div className="w-full">
+                  <InputForm
+                    name="Tên khách hàng:"
+                    type="text"
+                    className="border border-gray-300 bg-white text-black focus:border-primary"
+                    placeholder="Tên của bạn"
+                    classNameLabel="bg-white dark:peer-placeholder-shown:text-black dark:peer-focus:text-black"
+                  />
+                </div>
+              </div>
+              <Textarea
+                name="Lời nhắn:"
+                className="border border-gray-300 bg-white pb-20 text-black placeholder:text-lg placeholder:text-primary focus:border-primary focus:outline-none"
+                placeholder="Chúng tôi luôn sẵn sàng giải đáp mọi câu hỏi của bạn!. Hãy để lại câu hỏi tại đây."
+              />
+              <div className="w-full">
+                <Button
+                  aria-label="Nút: Gửi"
+                  className="w-full bg-primary text-sm text-white hover:border-primary hover:bg-secondary hover:text-white dark:hover:bg-opacity-50"
+                  type="submit"
+                >
+                  Gửi
+                </Button>
+                <span>{result}</span>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
