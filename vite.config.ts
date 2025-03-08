56d7025384fbce1f271f576241202bd32bc57f6a
q-dat <quocdastore79@gmail.com>
2025-03-08 10:48:22 +0700

    server site

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://api.7teck.vn/', // Proxy cho API
+      '/api': {
+        target: 'http://api.7teck.vn',
+        changeOrigin: true,
+        secure: false
+      }, 
       '/socket.io': {
-        target: 'http://api.7teck.vn/', // Đảm bảo WebSocket được xử lý
+        target: 'http://api.7teck.vn',
         ws: true, // Bật hỗ trợ WebSocket
         changeOrigin: true // Thay đổi Origin của request để phù hợp với backend
       }
