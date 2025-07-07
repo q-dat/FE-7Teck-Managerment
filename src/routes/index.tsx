98ec2d97275d6628f8b955d1b7a2be752e225d4b
q-dat <quocdastore79@gmail.com>
2025-07-07 19:24:23 +0700

    chore: AppRouter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,87 +5,53 @@ import PrivateRoute from './PrivateRoute';
 import LoginPage from '../pages/auth/LoginPage';
 
 // UserPage
-const User = lazy(() => import('../pages/user/User'));
-const HomePageSEO = lazy(() => import('../SEO/HomePageSEO'));
-const WarrantyPageSEO = lazy(() => import('../SEO/WarrantyPageSEO'));
-const PurchasePageSEO = lazy(() => import('../SEO/PurchasePageSEO'));
-const PhonePageSEO = lazy(() => import('../SEO/products/PhonePageSEO'));
-const TabletPageSEO = lazy(() => import('../SEO/products/TabletPageSEO'));
-const MacbookPageSEO = lazy(() => import('../SEO/products/MacbookPageSEO'));
-const WindowsPageSEO = lazy(() => import('../SEO/products/WindowsPageSEO'));
-const PriceListPageSEO = lazy(() => import('../SEO/PriceListPageSEO'));
-const NewsPageSEO = lazy(() => import('../SEO/post/NewsPageSEO'));
-const PostDetailSEO = lazy(() => import('../SEO/post/PostDetailSEO'));
-const TipsAndTricksPageSEO = lazy(
-  () => import('../SEO/post/TipsAndTricksPageSEO')
-);
-const GalleryPageSEO = lazy(() => import('../SEO/GalleryPageSEO'));
-//----------------------------------------------------------------
-const UsedProductsPage = lazy(
-  () => import('../pages/user/UsedProductsPage/UsedProductsPage')
-);
-const UsedPhoneByCatalogPageSEO = lazy(
-  () => import('../SEO/usedProductsByCatalog/UsedPhoneByCatalogPageSEO')
-);
-const UsedTabletByCatalogPageSEO = lazy(
-  () => import('../SEO/usedProductsByCatalog/UsedTabletByCatalogPageSEO')
-);
-const UsedMacbookByCatalogPageSEO = lazy(
-  () => import('../SEO/usedProductsByCatalog/UsedMacbookByCatalogPageSEO')
-);
-const UsedWindowsByCatalogPageSEO = lazy(
-  () => import('../SEO/usedProductsByCatalog/UsedWindowsByCatalogPageSEO')
-);
-//
-const PhoneDetailPageSEO = lazy(
-  () => import('../SEO/productDetails/PhoneDetailPageSEO')
-);
-const TabletDetailPageSEO = lazy(
-  () => import('../SEO/productDetails/TabletDetailPageSEO')
-);
-const WindowsDetailPageSEO = lazy(
-  () => import('../SEO/productDetails/WindowsDetailPageSEO')
-);
-const MacbookDetailPageSEO = lazy(
-  () => import('../SEO/productDetails/MacbookDetailPageSEO')
-);
+// const User = lazy(() => import('../pages/user/User'));
+// const HomePage = lazy(() => import('../pages/user/HomePage/HomePage'));
+// const WarrantyPage = lazy(() => import('../pages/user/WarrantyPage'));
+// const PurchasePage = lazy(() => import('../pages/user/PurchasePage'));
+// const PhonePage = lazy(() => import('../pages/user/ProductsPage/PhonePage'));
+// const TabletPage = lazy(() => import('../pages/user/ProductsPage/TabletPage'));
+// const MacbookPage = lazy(() => import('../pages/user/ProductsPage/MacbookPage'));
+// const WindowsPage = lazy(() => import('../pages/user/ProductsPage/WindowsPage'));
+// const PriceListPage = lazy(() => import('../pages/user/PriceListPage'));
+// const NewsPage = lazy(() => import('../pages/user/PostPage/NewsPage'));
+// const PostDetail = lazy(() => import('../pages/user/PostPage/PostDetail'));
+// const TipsAndTricksPage = lazy(() => import('../pages/user/PostPage/TipsAndTricksPage'));
+// const GalleryPage = lazy(() => import('../pages/user/GalleryPage'));
+// //----------------------------------------------------------------
+// const UsedProductsPage = lazy(() => import('../pages/user/UsedProductsPage/UsedProductsPage'));
+// const UsedPhoneByCatalogPage = lazy(() => import('../pages/user/usedProductsByCatalog/UsedPhoneByCatalogPage'));
+// const UsedTabletByCatalogPage = lazy(() => import('../pages/user/usedProductsByCatalog/UsedTabletByCatalogPage'));
+// const UsedMacbookByCatalogPage = lazy(() => import('../pages/user/usedProductsByCatalog/UsedMacbookByCatalogPage'));
+// const UsedWindowsByCatalogPage = lazy(() => import('../pages/user/usedProductsByCatalog/UsedWindowsByCatalogPage'));
+// //
+// const PhoneDetailPage = lazy(() => import('../pages/user/DetailsPage/PhoneDetailPage'));
+// const TabletDetailPage = lazy(() => import('../pages/user/DetailsPage/TabletDetailPage'));
+// const WindowsDetailPage = lazy(() => import('../pages/user/DetailsPage/WindowsDetailPage'));
+// const MacbookDetailPage = lazy(() => import('../pages/user/DetailsPage/MacbookDetailPage'));
 
 // admin
 const Admin = lazy(() => import('../cms/admin/Admin'));
 const DashboardPage = lazy(() => import('../cms/admin/DashboardPage'));
-const PhoneCatalogManager = lazy(
-  () => import('../cms/admin/PhoneCatalogManager')
-);
+const PhoneCatalogManager = lazy(() => import('../cms/admin/PhoneCatalogManager'));
 const PhoneManager = lazy(() => import('../cms/admin/PhoneManager'));
 
-const TabletCatalogManager = lazy(
-  () => import('../cms/admin/TabletCatalogManager')
-);
+const TabletCatalogManager = lazy(() => import('../cms/admin/TabletCatalogManager'));
 const TabletManager = lazy(() => import('../cms/admin/TabletManager'));
-const MacbookCatalogManager = lazy(
-  () => import('../cms/admin/MacbookCatalogManager')
-);
+const MacbookCatalogManager = lazy(() => import('../cms/admin/MacbookCatalogManager'));
 const MacbookManager = lazy(() => import('../cms/admin/MacbookManager'));
-const WindowsCatalogManager = lazy(
-  () => import('../cms/admin/WindowsCatalogManager')
-);
+const WindowsCatalogManager = lazy(() => import('../cms/admin/WindowsCatalogManager'));
 const WindowsManager = lazy(() => import('../cms/admin/WindowsManager'));
 
 // post
 const Post = lazy(() => import('../cms/admin/post/Post'));
 const PostManagerPage = lazy(() => import('../cms/admin/post/PostManagerPage'));
-const PostCatalogManagerPage = lazy(
-  () => import('../cms/admin/post/PostCatalogManagerPage')
-);
-const PriceListManagerPage = lazy(
-  () => import('../cms/admin/post/PriceListManagerPage')
-);
+const PostCatalogManagerPage = lazy(() => import('../cms/admin/post/PostCatalogManagerPage'));
+const PriceListManagerPage = lazy(() => import('../cms/admin/post/PriceListManagerPage'));
 
 // gallery
 const Gallery = lazy(() => import('../cms/admin/gallery/Gallery'));
-const GalleryManagerPage = lazy(
-  () => import('../cms/admin/gallery/GalleryManagerPage')
-);
+const GalleryManagerPage = lazy(() => import('../cms/admin/gallery/GalleryManagerPage'));
 
 // not found page
 const NotFound = lazy(() => import('../pages/404/NotFound'));
@@ -96,67 +62,34 @@ export default function AppRoutes() {
       <Routes>
         {/* UserPage  */}
         <Route element={<DefaultLayout />}>
-          <Route path="/" element={<User />}>
-            <Route index path="" element={<HomePageSEO />} />
+          <Route path="/" element={<LoginPage />}>
+            {/* <Route index path="" element={<HomePage />} /> */}
             {/*  */}
-            <Route path="dien-thoai" element={<PhonePageSEO />} />
-            <Route path="may-tinh-bang" element={<TabletPageSEO />} />
-            <Route path="macbook" element={<MacbookPageSEO />} />
-            <Route path="windows" element={<WindowsPageSEO />} />
+            {/* <Route path="dien-thoai" element={<PhonePage />} />
+            <Route path="may-tinh-bang" element={<TabletPage />} />
+            <Route path="macbook" element={<MacbookPage />} />
+            <Route path="windows" element={<WindowsPage />} /> */}
             {/* Used Products */}
-            <Route
-              path="thiet-bi-da-qua-su-dung"
-              element={<UsedProductsPage />}
-            />
-            <Route
-              path="dien-thoai/:catalog"
-              element={<UsedPhoneByCatalogPageSEO />}
-            />
-            <Route
-              path="may-tinh-bang/:catalog"
-              element={<UsedTabletByCatalogPageSEO />}
-            />
-            <Route
-              path="macbook/:catalog"
-              element={<UsedMacbookByCatalogPageSEO />}
-            />
-            <Route
-              path="windows/:catalog"
-              element={<UsedWindowsByCatalogPageSEO />}
-            />
+            {/* <Route path="thiet-bi-da-qua-su-dung" element={<UsedProductsPage />} />
+            <Route path="dien-thoai/:catalog" element={<UsedPhoneByCatalogPage />} />
+            <Route path="may-tinh-bang/:catalog" element={<UsedTabletByCatalogPage />} />
+            <Route path="macbook/:catalog" element={<UsedMacbookByCatalogPage />} />
+            <Route path="windows/:catalog" element={<UsedWindowsByCatalogPage />} /> */}
             {/*  */}
-            <Route
-              path="dien-thoai/:catalog/:id"
-              element={<PhoneDetailPageSEO />}
-            />
-            <Route
-              path="dien-thoai/:name/:id"
-              element={<PhoneDetailPageSEO />}
-            />
-            <Route
-              path="may-tinh-bang/:name/:id"
-              element={<TabletDetailPageSEO />}
-            />
-            <Route
-              path="windows/:name/:id"
-              element={<WindowsDetailPageSEO />}
-            />
-            <Route
-              path="macbook/:name/:id"
-              element={<MacbookDetailPageSEO />}
-            />
+            {/* <Route path="dien-thoai/:catalog/:id" element={<PhoneDetailPage />} />
+            <Route path="dien-thoai/:name/:id" element={<PhoneDetailPage />} />
+            <Route path="may-tinh-bang/:name/:id" element={<TabletDetailPage />} />
+            <Route path="windows/:name/:id" element={<WindowsDetailPage />} />
+            <Route path="macbook/:name/:id" element={<MacbookDetailPage />} /> */}
             {/*  */}
-            <Route path="tin-tuc-moi-nhat" element={<NewsPageSEO />} />
-            <Route path="tin-tuc/:title/:id" element={<PostDetailSEO />} />
+            {/* <Route path="tin-tuc-moi-nhat" element={<NewsPage />} />
+            <Route path="tin-tuc/:title/:id" element={<PostDetail />} /> */}
             {/*  */}
-            <Route
-              path="thu-thuat-va-meo-hay"
-              element={<TipsAndTricksPageSEO />}
-            />
-            <Route path="thanh-toan" element={<PurchasePageSEO />} />
-            <Route path="hanh-trinh-khach-hang" element={<GalleryPageSEO />} />
-            <Route path="bang-gia-thu-mua" element={<PriceListPageSEO />} />
-            <Route path="chinh-sach-bao-hanh" element={<WarrantyPageSEO />} />
+            {/* <Route path="thu-thuat-va-meo-hay" element={<TipsAndTricksPage />} />
+            <Route path="thanh-toan" element={<PurchasePage />} />
+            <Route path="hanh-trinh-khach-hang" element={<GalleryPage />} />
+            <Route path="bang-gia-thu-mua" element={<PriceListPage />} />
+            <Route path="chinh-sach-bao-hanh" element={<WarrantyPage />} /> */}
           </Route>
         </Route>
 
@@ -171,25 +104,13 @@ export default function AppRoutes() {
             }
           >
             <Route index path="" element={<DashboardPage />} />
-            <Route
-              path="phone-catalog-manager"
-              element={<PhoneCatalogManager />}
-            />
+            <Route path="phone-catalog-manager" element={<PhoneCatalogManager />} />
             <Route path="phone-manager" element={<PhoneManager />} />
-            <Route
-              path="tablet-catalog-manager"
-              element={<TabletCatalogManager />}
-            />
+            <Route path="tablet-catalog-manager" element={<TabletCatalogManager />} />
             <Route path="tablet-manager" element={<TabletManager />} />
-            <Route
-              path="macbook-catalog-manager"
-              element={<MacbookCatalogManager />}
-            />
+            <Route path="macbook-catalog-manager" element={<MacbookCatalogManager />} />
             <Route path="macbook-manager" element={<MacbookManager />} />
-            <Route
-              path="windows-catalog-manager"
-              element={<WindowsCatalogManager />}
-            />
+            <Route path="windows-catalog-manager" element={<WindowsCatalogManager />} />
             <Route path="windows-manager" element={<WindowsManager />} />
           </Route>
         </Route>
@@ -206,14 +127,8 @@ export default function AppRoutes() {
           >
             <Route index path="" element={<PostManagerPage />} />
             <Route path="post-manager" element={<PostManagerPage />} />
-            <Route
-              path="post-catalog-manager"
-              element={<PostCatalogManagerPage />}
-            />{' '}
-            <Route
-              path="price-list-manager"
-              element={<PriceListManagerPage />}
-            />
+            <Route path="post-catalog-manager" element={<PostCatalogManagerPage />} />{' '}
+            <Route path="price-list-manager" element={<PriceListManagerPage />} />
           </Route>
         </Route>
 
