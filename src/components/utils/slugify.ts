764ad1c2611c6d6b1457caeb21647cf6cee3dbda
q-dat <quocdatstore79@gmail.com>
2026-02-24 19:56:31 +0700

    feat: handle slugify

--- src/components/utils/slugify.ts
+++ src/components/utils/slugify.ts
@@ -1,9 +1,27 @@
-export const slugify = (text: string) => {
-  return text
-    .toString()
-    .normalize('NFD') // Chuyển sang Unicode
-    .replace(/\p{Diacritic}/gu, '') // Loại bỏ dấu
-    .toLowerCase() // Chuyển tất cả thành chữ thường
-    .replace(/[^a-z0-9]+/g, '-') // Thay thế khoảng trắng và ký tự không phải chữ cái bằng dấu gạch ngang
-    .replace(/^-+|-+$/g, ''); // Loại bỏ dấu gạch ngang ở đầu và cuối chuỗi
+import slugifyLib from 'slugify';
+
+export const slugify = (text: string): string => {
+  const normalized = text
+    .replace(/[\/|_]/g, '-') // thay các ký tự phân cách phổ biến
+    .replace(/\s+/g, ' ') // normalize space
+    .normalize('NFD') // tách ký tự có dấu thành ký tự thường + dấu
+    .replace(/[\u0300-\u036f]/g, ''); // loại bỏ dấu
+
+  return slugifyLib(normalized, {
+    lower: true,
+    strict: true,
+    trim: true,
+    locale: 'vi'
+  });
 };
+
+// const testCases: string[] = [
+//   'OPPO Reno14 F 5G 12GB/256GB',
+//   'iPhone 15 Pro Max 256GB',
+//   'Samsung Galaxy S24 Ultra 12GB/1TB',
+//   'Xiaomi 14T Pro (5G) 8GB/512GB'
+// ];
+
+// testCases.forEach(value => {
+//   alert(value + ' => ' + slugify(value));
+// });
