f320a23f940d0ebee8472ead87d14bc2b4947bb3
q-dat <quocdatstore79@gmail.com>
2026-03-06 00:10:51 +0700

    chore: PhoneCatalogItemFullUpdateModal UI

--- src/components/adminPage/modalAdmin/phone/PhoneCatalogModal/PhoneCatalogItemFullUpdateModal.tsx
+++ src/components/adminPage/modalAdmin/phone/PhoneCatalogModal/PhoneCatalogItemFullUpdateModal.tsx
@@ -89,124 +89,113 @@ const PhoneCatalogItemFullUpdateModal: React.FC<ModalUpdateFullPhoneCatalogProps
           value={jsonInput}
           onChange={e => setJsonInput(e.target.value)}
           placeholder={`Dán JSON ở đây... (Copy từ Excel hoặc file mẫu)
-            Ví dụ:
+        Ví dụ:
             {
-            "price": 0,
+            "name": "Xiaomi Redmi 14c 4GB/128GB",
+            "slug": "xiaomi-redmi-14c-4gb-128gb",
+            "img": "https://res.cloudinary.com/cloud7teck/image/upload/1/123.jpg",
+            "price": 3360,
             "status": 0,
-            "content": "",
+            "content": "New",
             "configuration_and_memory": {
-                "operating_system": "Xiaomi HyperOS (Android 14)",
-                "cpu_chip": "MediaTek Helio G91 Ultra 8 nhân",
-                "cpu_speed": "2 nhân 2.0 GHz & 6 nhân 1.8 GHz",
-                "gpu": "Mali-G52 MC2",
-                "ram": "6 GB",
-                "storage_capacity": "128 GB",
-                "remaining_capacity": "Khoảng 115 GB",
-                "memory_card": "MicroSD, hỗ trợ tối đa 1 TB",
-                "contacts": "Không giới hạn"
+              "operating_system": "Android 14",
+              "cpu_chip": "MediaTek Helio G81-Ultra 8 nhân",
+              "cpu_speed": "2 GHz",
+              "gpu": "Mali-G52 MC2",
+              "ram": "4GB",
+              "storage_capacity": "128GB",
+              "remaining_capacity": "115 GB",
+              "memory_card": "MicroSD, hỗ trợ tối đa 1 TB",
+              "contacts": "Không giới hạn"
             },
             "camera_and_screen": {
-                "rear_camera_resolution": "Chính 108 MP & Phụ 2 MP",
-                "rear_camera_video": [
+              "rear_camera_resolution": "Chính 50 MP & Phụ QVGA",
+              "rear_camera_video": [
                 "HD 720p@30fps",
                 "FullHD 1080p@30fps"
-                ],
-                "rear_camera_flash": "Có",
-                "rear_camera_features": [
-                "Zoom kỹ thuật số",
+              ],
+              "rear_camera_flash": "Có",
+              "rear_camera_features": [
                 "Xóa phông",
                 "Trôi nhanh thời gian (Time Lapse)",
                 "Siêu độ phân giải",
-                "Siêu cận (Macro)",
-                "Làm đẹp",
                 "HDR",
                 "Ban đêm (Night Mode)"
-                ],
-                "front_camera_resolution": "13 MP",
-                "front_camera_features": [
+              ],
+              "front_camera_resolution": "13 MP",
+              "front_camera_features": [
                 "Xóa phông",
                 "Trôi nhanh thời gian (Time Lapse)",
-                "Quay video HD",
-                "Quay video Full HD",
-                "Làm đẹp",
                 "HDR",
-                "Flash màn hình"
-                ],
-                "screen_technology": "IPS LCD",
-                "screen_resolution": "Full HD+ (1080 x 2460 Pixels)",
-                "screen_size": "6.79\\" - 90 Hz",
-                "max_brightness": "550 nits",
-                "touchscreen_glass": "Corning Gorilla Glass 3"
+                "Chụp đêm"
+              ],
+              "screen_technology": "IPS LCD",
+              "screen_resolution": "HD+ (720 x 1640 Pixels)",
+              "screen_size": "6.88\"",
+              "max_brightness": "600 nits",
+              "touchscreen_glass": "Kính cường lực Corning Gorilla Glass 3"
             },
             "battery_and_charging": {
-                "battery_capacity": "5030 mAh",
-                "battery_type": "Li-Po",
-                "max_charging_support": "33 W",
-                "battery_technology": [
+              "battery_capacity": "5160 mAh",
+              "battery_type": "Li-Po",
+              "max_charging_support": "18 W",
+              "battery_technology": [
                 "Tiết kiệm pin",
                 "Sạc pin nhanh"
-                ]
+              ]
             },
             "features": {
-                "advanced_security": [
+              "advanced_security": [
                 "Mở khoá vân tay cạnh viền",
                 "Mở khoá khuôn mặt"
-                ],
-                "special_features": [
+              ],
+              "special_features": [
                 "Mở rộng bộ nhớ RAM"
-                ],
-                "water_dust_resistant": "IP53",
-                "voice_recording": [
+              ],
+              "water_dust_resistant": "IP54 (Ví dụ)", 
+              "voice_recording": [
                 "Ghi âm mặc định",
                 "Ghi âm cuộc gọi"
-                ],
-                "radio": [
+              ],
+              "radio": [
                 "Có"
-                ],
-                "video_playback": "WEBM, TS, MP4, MKV, AVI, 3GP",
-                "music_playback": [
-                "XMF",
-                "WAV",
-                "RTX",
-                "OTA",
+              ],
+              "video_playback": "Có",
+              "music_playback": [
                 "OGG",
-                "OGA",
                 "Midi",
                 "MP3",
-                "M4A",
-                "IMY",
-                "FLAC",
-                "AWB",
-                "AMR",
-                "AAC"
-                ]
+                "FLAC"
+              ]
             },
             "connectivity": {
-                "mobile_network": "Hỗ trợ 4G",
-                "sim": "2 Nano SIM (SIM 2 chung khe thẻ nhớ)",
-                "wifi": [
+              "mobile_network": "Hỗ trợ 4G",
+              "sim": "2 Nano SIM",
+              "wifi": [
                 "Wi-Fi hotspot",
+                "Wi-Fi Direct",
+                "Wi-Fi 802.11 a/b/g/n/ac",
                 "Dual-band (2.4 GHz/5 GHz)"
-                ],
-                "gps": [
+              ],
+              "gps": [
                 "GPS",
                 "GLONASS",
                 "GALILEO",
                 "BEIDOU"
-                ],
-                "bluetooth": "v5.4",
-                "charging_connection_port": "Type-C",
-                "headphone_jack": "3.5 mm",
-                "other_connectivity": "Hồng ngoại"
+              ],
+              "bluetooth": "5.4",
+              "charging_connection_port": "Type-C",
+              "headphone_jack": "3.5 mm",
+              "other_connectivity": "OTG"
             },
             "design_and_material": {
-                "design": "Nguyên khối",
-                "material": "Khung & Mặt lưng nhựa",
-                "dimensions_and_weight": "168.6 x 76.28 x 8.3 mm - 198 g",
-                "release_date": "03/2025",
-                "brand": "Xiaomi"
+              "design": "Nguyên khối",
+              "material": "Khung & Mặt lưng nhựa",
+              "dimensions_and_weight": "Dài 171.88 mm - Ngang 77.8 mm - Dày 8.22 mm - Nặng 207 g",
+              "release_date": "08/2024",
+              "brand": "Xiaomi"
             }
-            }`}
+          }`}
           bordered
         />
 
